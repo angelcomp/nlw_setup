6a083576c2582cdfb34450fadeb75b5e80d49c5f
Angelica dos Santos <santosangelicassp@gmail.com>
2023-01-20 09:10:33 -0300

    component habitDay

---
+++ mobile/src/components/HabitDayPlaceholder.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native'
+import { DAY_SIZE } from './HabitDay'
+
+export function HabitDaysPlaceHolder() {
+    return (
+        <View 
+            className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40'
+            style={{width: DAY_SIZE, height: DAY_SIZE}}
+        ></View>
+    )
+}
+
+export default HabitDaysPlaceHolder
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,11 +1,48 @@
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, ScrollView } from "react-native"
+import { DAY_SIZE, HabitDay } from "../components/HabitDay"
+import { generateDatesFromYear } from "../utils/generate-dates-from-year"
 import { Header } from "../components/Header"
+import HabitDaysPlaceHolder from "../components/HabitDayPlaceholder"
 
+const WEEK_DAYS = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+
+const summaryDates = generateDatesFromYear()
+
+const minimumSummaryDataSize = 18 * 7
+const amountOfDaysToFill= minimumSummaryDataSize - summaryDates.length
 
 export function Home() {
     return (
         <View className="flex-1 bg-background px-8 pt-16">
            <Header/>
+
+            <View className="w-full flex-row justify-between mt-6 mb-2">
+                {
+                    WEEK_DAYS.map((weekDay, i) => (
+                        <Text key={i}
+                            className="text-zinc-400 text-xl font-bold"
+                            style={{width: DAY_SIZE}}
+                        >{weekDay}</Text>
+                    ))
+                }
+            </View>
+
+            <ScrollView contentContainerStyle={{paddingBottom: 50}}>
+                <View className="flex-row flex-wrap">
+                        {
+                            summaryDates.map(date => {
+                                return <HabitDay key={date.toString()}/>
+                            })
+                        }
+                        {
+                            amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map( (_, i) => {
+                                return (
+                                    <HabitDaysPlaceHolder key={`${i}`}/>
+                                    )
+                                })
+                            }
+                </View>
+            </ScrollView>
         </View>
     )
 }
